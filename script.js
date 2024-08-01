2cf0272b68b90275362a161d9674377d11858082
M Abdul Hafidz <hfidz7534@gmail.com>
2024-08-01 11:08:34 +0700

    second commit

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon .classList.toggle("open")
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const observerOptions =  {
+        threshold : 0.1
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target)
+            }
+        });
+    };
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    document.querySelectorAll('.skills-wrapper').forEach(wrapper => {
+        observer.observe(wrapper);
+    });
+});
